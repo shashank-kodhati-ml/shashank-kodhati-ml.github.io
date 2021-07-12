6298bc56b39b67ac4afb4615f0bdca6e06d16709
shashank-kodhati <shashankkodhati@gmail.com>
2021-07-13 01:13:37 +0530

    Updates

---
+++ precache-manifest.65acbe827a15dd4559c340389f29b7b5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b6f0664ecaea67ff0138ff3754e448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47c34a8db786151314d7",
+    "url": "/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "40fde1456711a97ff6fb",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "57371bfd100f63ad50d2",
+    "url": "/static/css/main.43e5ba24.chunk.css"
+  },
+  {
+    "revision": "10d1cc1a3f6094026801",
+    "url": "/static/js/2.914c76e0.chunk.js"
+  },
+  {
+    "revision": "47c34a8db786151314d7",
+    "url": "/static/js/3.7686ae46.chunk.js"
+  },
+  {
+    "revision": "40fde1456711a97ff6fb",
+    "url": "/static/js/4.9b1149ee.chunk.js"
+  },
+  {
+    "revision": "57371bfd100f63ad50d2",
+    "url": "/static/js/main.185b7cee.chunk.js"
+  },
+  {
+    "revision": "0cc62095a73b2ffdc92c",
+    "url": "/static/js/runtime-main.3e02f1b1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1353a648507e3742b0c91cdf1fc583da",
+    "url": "/static/media/PIC.1353a648.jpeg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "209dc70cb4e55ef15d68b39434772cee",
+    "url": "/static/media/data-engineer-3.209dc70c.png"
+  },
+  {
+    "revision": "cfac5e91b21783ab8fbabd579be3a977",
+    "url": "/static/media/data-engineer-5.cfac5e91.jpg"
+  },
+  {
+    "revision": "992399a95bac6f96927db764688707fd",
+    "url": "/static/media/data-engineer-8.992399a9.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "bdfb454ed23161c574be777917891d80",
+    "url": "/static/media/scikit-learn-logo-8766D07E2E-seeklogo.com.bdfb454e.png"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
